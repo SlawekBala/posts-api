1ad6f926059d4b0d079a15aac7d323754f4bf7eb
Slawek Bala <slawek_bala@wp.pl>
2021-05-21 20:19:01 +0200

    Add Login Controller

--- src/controllers/DeletePostController.js
+++ src/controllers/DeletePostController.js
@@ -4,6 +4,7 @@ exports.deletePost = async (req, res) => {
 
   const post = await Posts.query().findOne(req.params).delete();
 
+  res.send(req.params);
   console.log(req.params);
   console.log(post, ' WAS DELETED');
 };
@@ -12,6 +13,7 @@ exports.deletePosts = async (req, res) => {
 
   const post = await Posts.query().whereBetween('id', [req.params.uuidfrom, req.params.uuidto]).delete();
 
+  res.send(req.params)
   console.log(req.params);
   console.log(post, ' WERE DELETED');
 }
