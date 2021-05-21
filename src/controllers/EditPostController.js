f766eb1a19be4ff582c2169047e7d32676b87f3b
Slawek Bala <slawek_bala@wp.pl>
2021-05-21 20:09:18 +0200

    Add Login Controller

--- src/controllers/EditPostController.js
+++ src/controllers/EditPostController.js
@@ -2,25 +2,12 @@ import Posts from '../models/Posts'
 
 exports.editPost = async (req, res) => { 
   
-  // const editRecord = await Posts.query().patchAndFetchById(req.params,
-  //   {  title: req.body.title,
-  //       lead: req.body.lead,
-  //       content: req.body.content
-  //   });
-  // .patch({
-  //   title: req.body.title,
-  //   lead: req.body.lead,
-  //   content: req.body.content
-  // }).findOne(req.params);
-
-  const findPost = await Posts.query().findOne(req.params);
-  const updatedPost = findPost.$query().patch({
+   const updatedPost = await Posts.query().patch({
     title: req.body.title,
     lead: req.body.lead,
     content: req.body.content
-  });
+  }).findOne(req.params);
 
-  console.log(findPost)
   console.log(updatedPost)
   res.json(req.body)
 };
